c4adb28a2532bb41b90a1fe0b9714a36411dac57
Lior <lior05328@gmail.com>
2019-12-21 23:47:03 +0200

    BE

---
+++ project_dotNet5780_1766_0406/BE/order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class order
+    {
+        public double _HostingUnitKey { get; }//number Hosting Unit
+        public double _GuestRequestKey { get; }//number Guest Request
+        public double _OrderKey { get; }//number order
+
+        private Status _status_inve { get; }//status order
+        private string _CreateDate { get; }//day:_ _.moth:_ _.year:_ _ _ _
+        private string _OrderDate { get; } //day:_ _.moth:_ _.year:_ _ _ _
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
